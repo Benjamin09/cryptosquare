f9b2bb95b6601f1bed9a0081e5e6ec55ac11dfeb
Benjamin Heflin and Wolfgang Warneke <benjamin@>
2016-06-09 14:35:19 -0700

    add condition for amounts equal to perfect square

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
   $("#crypto").submit(function(event) {
     var message = $("#message").val();
     var squareVal = Math.floor(Math.sqrt(message.length)) + 1;
+    if ( Math.sqrt(message.length) % 1 === 0 ) {
+      squareVal -= 1;
+    }
     alert(squareVal);
     event.preventDefault();
   });

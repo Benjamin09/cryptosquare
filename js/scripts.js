0053e47232f151cff74ac023608742d912928cff
Benjamin Heflin and Wolfgang Warneke <benjamin@>
2016-06-09 14:58:39 -0700

    add loop to parse message into arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
+var squareColumns = [];
+var i;
+//for testing
 
 $(document).ready(function() {
   $("#crypto").submit(function(event) {
+    squareColumns = [];
     var message = $("#message").val();
+    //this place for taking away spaces and punctuation
     var squareVal = Math.floor(Math.sqrt(message.length)) + 1;
     if ( Math.sqrt(message.length) % 1 === 0 ) {
       squareVal -= 1;
     }
-    alert(squareVal);
+    for (i = 0; i <= squareVal; ++i) {
+      squareColumns[i] = [];
+    }
+    squareColumns.forEach(function(column) {
+      for (i = 0; i < squareVal; ++i) {
+        column[i] = message.substr(0,1);
+        message = message.substr(1);
+      }
+    });
     event.preventDefault();
   });
 });

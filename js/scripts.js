38dc4f6613eb62d591cc82f106d7a220e7dfa237
Benjamin Heflin and Wolfgang Warneke <wolfgang@>
2016-06-09 14:30:56 -0700

    add variable to get necessary length of square

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-alert('working');
+
+$(document).ready(function() {
+  $("#crypto").submit(function(event) {
+    var message = $("#message").val();
+    var squareVal = Math.floor(Math.sqrt(message.length)) + 1;
+    alert(squareVal);
+    event.preventDefault();
+  });
+});
